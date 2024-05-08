7985359eb10945ca4ed5a149f49ac3122036f7e9
dsfgsdg2dfghdf3 <js1rettis12@hotmail.com>
2024-05-09 00:00:07 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-  if (left && right) return root;
-  return left ? left : right;
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
