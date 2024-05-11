2e770e9eb1974ea109c0b2c5bf4e95d58cf10c5c
dsfgsdg2dfghdf3 <js1rettis12@hotmail.com>
2024-05-12 04:58:20 +0700

    demo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
+function lengthOfLongestSubstring(s) {
+  if (s.length === 0) return 0;
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
-  return arr;
+  return maxLength;
 }
